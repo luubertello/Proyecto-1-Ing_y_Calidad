43f8cf4c907df842bf4a3ec724f171912716f6c0
luubertello <lucianabertello8@gmail.com>
2026-08-24 20:38:05 -0300

    cambio en ssl para desplegar

--- Proyecto1_Back/proyecto/src/app.module.ts
+++ Proyecto1_Back/proyecto/src/app.module.ts
@@ -48,7 +48,9 @@ import { BusquedasModule } from './modules/gestion-documentos/busquedas/busqueda
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       // entities,
       synchronize: false,  
-      ssl: process.env.DB_SSL === 'true',
+      ssl: process.env.DB_SSL === 'true'
+        ? { rejectUnauthorized: false }
+        : false,
     }),
 
     MarcaModule,
